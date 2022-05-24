504755210d996139399175b42990a1a3cfc3a5a9
Connor Horman <chorman64@gmail.com>
2022-05-23 22:41:57 -0400

    Work on clever as

--- gas/config/tc-clever.c
+++ gas/config/tc-clever.c
@@ -4,7 +4,6 @@
 #include "dwarf2dbg.h"
 #include "opcode/clever.h"
 #include "elf/clever.h"
-#include "clever-opc.h"
 
 #include <limits.h>
 
@@ -12,10 +11,10 @@ const char *md_shortopts = "";
 int is_strict = 0;
 struct option md_longopts[] =
 {
-  {"strict-machine", optional_argument,&is_strict,}
+  {"strict-machine", optional_argument,&is_strict,1},
   {NULL, no_argument, NULL, 0}
 };
-
+size_t md_longopts_size = sizeof (md_longopts);
 
 union clever_extensions enabled_extensions = CLEVER_ALL_EXTENSIONS;
 
@@ -41,3 +40,153 @@ static const struct clever_machine_spec machines[] = {
 };
 
 const struct clever_machine_spec* mach = &machines[1];
+
+extern const struct clever_branch_info clever_ubranches[];
+extern const struct clever_branch_info clever_sbranches[];
+
+extern const struct clever_branch_info clever_cond_branch_info;
+
+extern const struct clever_instruction_info clever_insns[];
+
+/* Primary Opcode (non-specialization) hash table.  */
+static htab_t clever_primary_inst_hash;
+
+/* A copy of the original instruction (used in error messages).  */
+char ins_parse[MAX_INST_LEN];
+
+/* Round up a section size to the appropriate boundary.  */
+
+valueT
+md_section_align (segT seg, valueT val)
+{
+  /* Round .text sections to a multiple of 2.  */
+  if (strncmp(seg->name,".text",5))
+    return (val + 1) & ~1;
+  return val;
+}
+
+enum{
+    addr_size = 0,
+    reg_size = 1,
+    mem_size = 2,
+    imm_size = 3,
+}
+
+// Default size for various kinds of operands w/o size specifiers
+static int def_size[3] = {3,3,3,0};
+// Bitfield of sizes (0-3) for register operands that gpr specializations should not be applied to on
+static unsigned int no_spec_on;
+
+// Whether or not gpr destination (first operand) specializations are applied
+static _Bool no_left_spec;
+// Whether or not gpr source (second operand) specializations are applied
+static _Bool no_right_spec;
+
+// Whether or not relaxations should be generated for symbols
+static _Bool allow_relax = 1;
+// Whether or not CLEVER_RELAX_SHORT or CLEVER_RELAX_SHORT_PCREL should be generated
+static _Bool use_short_relax = 1;
+// Whether or not CLEVER_RELAX_SHORT should be generated
+static _Bool use_short_relax_abs = 1;
+// Whether or not CLEVER_RELAX_SHORT_PCREL should be generated
+static _Bool use_short_relax_rel = 1;
+
+static void relax_set(int val){
+#ifdef md_flush_pending_output
+  md_flush_pending_output ();
+#endif
+
+  SKIP_ALL_WHITESPACE();
+  if(strncmp(input_line_pointer,"short",5)==0){
+    input_line_pointer+=5;
+    SKIP_ALL_WHITESPACE();
+    if(strncmp(input_line_pointer,"rel",3)==0){
+        input_line_pointer+=3;
+        use_short_relax_rel = val;
+    }else if(strncmp(input_line_pointer, "abs",3)==0){
+        input_line_pointer+=3;
+        use_short_relax_rel = val;
+    }else{
+        use_short_relax = val;
+    }
+  }else{
+    allow_relax = val;
+  }
+  demand_empty_rest_of_line();
+}
+
+enum size_kind{
+    size_any = 0,
+    size_imm_only = 1,
+    size_no_imm = 2,
+    size_vector = 3,
+}
+
+struct size_names{
+    const char* name;
+    const char* bitwidth;
+    enum size_kind kind;
+    size_t ss;
+    size_t size_bytes;
+}
+
+const struct size_names names[] = {
+    {"byte","8",size_no_imm,0,1},
+    {"short","12",size_imm_only,0,2},
+    {"half","16", size_any,1,2},
+    {"single","32", size_any,2,4},
+    {"double","64", size_any, 3, 8},
+    {"vector", "128", size_vector, 4, 16},
+    {"quad", "128", size_vector, 4, 16},
+    {NULL,NULL,0,0,0}
+};
+
+const struct size_name* parse_size(int allow_numeric){
+    for(int i = 0;names[i].name;i++){
+        if(strncmp(names[i].name,input_line_pointer,strlen(names[i].name))==0){
+            input_line_pointer += strlen(names[i].name);
+            if(*input_line_pointer!=' '&&*input_line_pointer!='['&&!is_it_end_of_statement()){
+                input_line_pointer -= strlen(names[i].name);
+                return NULL;
+            }
+            return &names[i];
+        }else if(allow_numeric&&strncmp(names[i].bitwidth,input_line_pointer,strlen(names[i].bitwidth))==0){
+            input_line_pointer += strlen(names[i].bitwdith);
+            if(*input_line_pointer!=' '&&*input_line_pointer!='['&&!is_it_end_of_statement()){
+                input_line_pointer -= strlen(names[i].bitwdith);
+                return NULL;
+            }
+            return &names[i];
+        }
+    }
+    return NULL;
+}
+
+static void set_def_size(int off){
+    SKIP_ALL_WHITESPACE();
+    const struct size_name* size = parse_size(1);
+    if(!size)
+        as_bad(_("Invalid size control %s"),input_line_pointer);
+    if(off==imm_size&&size->kind==size_no_imm)
+        as_bad(_("Cannot use size %s as default immediate size"),size->name);
+    if(off!=imm_size&&size->kind==size_imm_only)
+        as_bad(_("Cannot use size %s as default immediate size"),size->name);
+    demand_empty_rest_of_line();
+}
+
+
+const pseudo_typeS md_pseudo_table[] =
+{
+  /* In CR16 machine, align is in bytes (not a ptwo boundary).  */
+  {"align", s_align_bytes, 0},
+  {"relax", relax_set, 1},
+  {"norelax", relax_set, 0},
+  {"addrsize",set_def_size, addr_size},
+  {"regsize", set_def_size, reg_size},
+  {"immsize", set_def_size, imm_size},
+  {"memsize", set_def_size, mem_size},
+  {"spec", set_spec, 1},
+  {"nospec", set_spec, 0},
+  {0, 0, 0}
+};
+
--- include/opcode/clever.h
+++ include/opcode/clever.h
@@ -1,6 +1,11 @@
 #ifndef _OPCODES_CLEVER_H_
 #define _OPCODES_CLEVER_H_
 
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
 typedef enum{
     r0,
     r1,
@@ -165,5 +170,165 @@ enum clever_condition{
 #define CLEVER_BRANCH_UNCONDITIONAL_SIZE(opcode) ((CLEVER_OP_H(opcode)&0x3)+1)
 
 #define CLEVER_MAX_OPERANDS 3
+
+// Extensions in Clever-ISA
+enum clever_extension{
+    XMain, // All CPUs support X-main by definition
+    XFloat,
+    XVec,
+    XFloatExt,
+    XRand,
+    XVirtualization,
+
+    ExtMax = XVirtualization
+};
+
+#define ExtensionNumUints \
+  (ExtMax / sizeof (unsigned int) / CHAR_BIT + 1)
+
+#define ExtensionNumBits (ExtensionNumUints * sizeof(unsigned int) * CHAR_BIT)
+
+#define ExtensionUnused (ExtMax + 1)
+
+union clever_extensions{
+    struct{
+        unsigned int x_main:1;
+        unsigned int x_float:1;
+        unsigned int x_vec:1;
+        unsigned int x_float_ext:1;
+        unsigned int x_rand:1;
+        unsigned int x_virtualization:1;
+        unsigned int unused_bits:(ExtensionNumBits-ExtensionUnused);
+    };
+    unsigned int flags[ExtensionNumUints];
+};
+
+#define CLEVER_HAS_EXTENSION(exts, xno) ((((exts)->flags)[(xno)/(sizeof (unsigned int)*CHAR_BIT)])&(0<<((xno)%(sizeof (unsigned int)*CHAR_BIT))))
+
+#define CLEVER_ALL_EXTENSIONS {.x_main = 1, .x_float = 1, .x_vec = 1, .x_float_ext = 1, .x_rand = 1, .x_virtualization = 1}
+
+// X-rand and X-virtualization are not part of the prepared 1.0 spec
+#define CLEVER1_0_ALL_EXTENSIONS {.x_main = 1, .x_float = 1, .x_vec = 1, .x_float_ext = 1}
+
+// X-main is mandatory, but other bits are unset.
+#define CLEVER_NO_EXTENSIONS {.x_main = 1}
+
+enum{
+    branch_addr = 16,
+    prefix,
+};
+
+enum clever_unconditional_branches{
+    jmp,
+    call,
+    fcall,
+    ret,
+    scall,
+    intr,
+    ijmp,
+    icall,
+    ifcall,
+    jsm,
+    callsm,
+    retrsm,
+    last_user_branch = retrsm,
+    user_branch_count,
+    // supervisor branches
+    first_super_branch = 32,
+    scret = 32,
+    reti,
+    last_basic_super_branch = reti,
+
+    next_super_branch = first_super_branch+11,
+    hcall = first_super_branch+11,
+    last_super_branch = hcall,
+    hyper_branches = first_super_branch+16,
+    first_hyper_branch = hyper_branches+6,
+    hret = hyper_branches+6,
+    hresume = hyper_branches+7,
+    last_hyper_branch = hresume,
+};
+
+#define BASIC_SUPER_BRANCH_COUNT ((last_basic_super_branch+1)-first_super_branch)
+#define VIRT_SUPER_BRANCH_COUNT ((last_super_branch+1)-next_super_branch)
+#define SUPER_BRANCH_COUNT (BASIC_SUPER_BRANCH_COUNT+VIRT_SUPER_BRANCH_COUNT)
+#define HYPER_BRANCH_COUNT ((last_hyper_branch+1)-first_hyper_branch)
+#define SBRANCHES_SIZE (BASIC_SUPER_BRANCH_COUNT+VIRT_SUPER_BRANCH_COUNT+HYPER_BRANCH_COUNT)
+
+
+/// Given the entire opcode, obtains the unconditional branch operand
+/// Returns 0 if no such 
+typedef unsigned int(clever_branch_dest_size)(uint16_t);
+/// Given the entire opcode, obtains the indirect branch target register if any
+/// returns 63 (reserved63) if none
+typedef clever_reg(clever_indirect_branch_register)(uint16_t);
+
+
+
+enum{
+    NullPrefix = 0x000, // not an actual prefix, but shares an opcode with und
+    Vec = 0x200,
+    Repbi = 0x028,
+    Repbc = 0x029,
+};
+
+typedef bool(clever_validate_control_field)(uint16_t, union clever_extensions*);
+typedef char*(clever_write_control_suffix)(uint16_t, char* c);
+
+typedef struct{
+    uint16_t size;
+    union{char* symbol; uint64_t value_hi;};
+    uint64_t value;
+} clever_immediate;
+
+
+typedef union clever_operand{
+    struct{ enum clever_operand_kind kind; bool vec; uint16_t size; clever_reg reg;} direct;
+    struct{ enum clever_operand_kind kind; uint16_t size; uint8_t scale; clever_reg base; uint8_t index_kind_and_val;} indirect;
+    struct{ enum clever_operand_kind kind; bool rel; clever_immediate imm;} short_imm;
+    struct{ enum clever_operand_kind kind; bool rel; bool mem; clever_immediate imm; uint16_t mem_size;} long_imm;
+} clever_operand;
+
+
+typedef size_t clever_insert_operands(uint16_t, clever_operand*, size_t);
+
+typedef bool clever_validate_operands(uint16_t, clever_operand*);
+
+struct clever_instruction_info{
+    uint16_t opcode;
+    uint16_t valid_prefix;
+    uint16_t operand_count;
+    uint16_t clever_extension;
+    const char* mnemonic;
+    clever_validate_control_field* validate_h;
+    clever_write_control_suffix* print_h;
+    clever_insert_operands* pre_insert_operands;
+    clever_insert_operands* post_insert_operands;
+    clever_validate_operands* validate_operands;
+    const struct clever_instruction_info* gpr_dest_spec;
+    const struct clever_instruction_info* gpr_src_spec;
+};
+
+
+struct clever_branch_info{
+    clever_branch_dest_size* dest_size;
+    clever_indirect_branch_register* reg;
+    bool allow_relative;
+    const struct clever_instruction_info* insn;
+};
+
+
+
+struct clever_instruction{
+    uint16_t prefix_opc;
+    uint16_t opc;
+    const struct clever_instruction_info* prefix_insn;
+    const struct clever_instruction_info* insn;
+    union clever_operand ops[CLEVER_MAX_OPERANDS];
+    size_t num_ops;
+};
+
+#define MAX_MNEMONIC_LENGTH 24
+
 
 #endif 
